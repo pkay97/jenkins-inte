e018889107b2167bd8d80f5041bdad74681322d6
pkay97 <007kumarpradeep008@gmail.com>
2025-05-23 10:41:37 +0530

    add another text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     <div className="text-[#4287f5]">
       <h1>Jenkins Work</h1>
       <p>another task done</p>
+      <p>deploy on vercel done</p>
     </div>
   );
 }
